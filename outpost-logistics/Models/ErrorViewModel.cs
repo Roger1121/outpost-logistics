b05397237660f776993983082b57669502c0afc7
Roger1121 <leszczynski_szymon@wp.pl>
2025-10-28 20:26:53 +0100

    init project

---
+++ outpost-logistics/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace outpost_logistics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
